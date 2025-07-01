1b23f705bb58bf112f3739bbf8ce3cf1b8007073
MaeLogan <mael.dabard@gmail.com>
2025-07-01 15:54:03 +0200

    add Place Model

---
+++ FindBar/Models/Place.cs
@@ -0,0 +1,9 @@
+namespace FindBar.Models;
+
+public class Place
+{
+    public string Name { get; set; }
+    public string Address { get; set; }
+    public double Latitude { get; set; }
+    public double Longitude { get; set; }
+}
